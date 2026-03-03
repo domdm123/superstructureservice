e36db044becbd325a3b5680bddd444dcafef1e95
Let Cleanness Be <let_cleanness_be@hotmail.com>
2026-03-03 13:10:35 +0000

    Remove star pattern background from PageHero for cleaner look

--- components/PageHero.tsx
+++ components/PageHero.tsx
@@ -45,15 +45,6 @@ export default function PageHero({
           <div className="absolute inset-0 bg-gradient-to-r from-[#111111]/80 via-[#111111]/60 to-[#111111]/80" />
         </div>
       )}
-      {/* Background pattern */}
-      <div className="absolute inset-0 opacity-5">
-        <div
-          className="absolute inset-0"
-          style={{
-            backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='1'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
-          }}
-        />
-      </div>
       {/* Gold accent bar */}
       <div className="absolute top-0 left-0 w-1 h-full bg-[#4a9ebb]" />
 
